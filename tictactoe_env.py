ceb8a17867970f044bfe06e5382f01633729bbdb
Neil Thomas <thomas.a.neil@gmail.com>
2017-11-24 16:33:47 -0800

    Some formatting fixes. Remove unused imports. Docstring conventions

--- tictactoe_env.py
+++ tictactoe_env.py
@@ -1,16 +1,14 @@
 import numpy as np
-import chess
 
-from tests.utils import map_xy_to_square, map_square_to_xy
 
 class TicTacToeEnv(object):
     def __init__(self):
-        '''
+        """
         state space: 2x3x3
         action space: 18 possible values -- 1 hot
             action: make actions a single integer -- in range(0, 18)
         assume x's always go first
-        '''
+        """
         self.state = np.zeros((2, 3, 3))
         self.action_dims = (2, 3, 3)
 
@@ -43,20 +41,22 @@ class TicTacToeEnv(object):
             return 1
 
     def outcome(self, state):
-        '''
+        """
         1 is a win for x's
-        -1 is a win for o's 
-        0 is a tie 
+        -1 is a win for o's
+        0 is a tie
         2: the game is not over
-        '''
+        """
         for turn in range(2):
-            if (state[turn,0,0] == 1 and state[turn,1,1] == 1 and state[turn,2,2] == 1) or (state[turn,2,0] == 1 and state[turn,1,1] == 1 and state[turn,0,2] == 1):
+            if all([state[turn, 0, 0] == 1, state[turn, 1, 1] == 1, state[turn, 2, 2] == 1]):
+                return 1
+            if all([state[turn, 2, 0] == 1, state[turn, 1, 1] == 1, state[turn, 0, 2] == 1]):
                 return 1
             for i in range(3):
-                if state[turn,0,i] == 1 and state[turn,1,i] == 1 and state[turn,2,i] == 1:
+                if state[turn, 0, i] == 1 and state[turn, 1, i] == 1 and state[turn, 2, i] == 1:
                     return 1
             for j in range(3):
-                if state[turn,j,0] == 1 and state[turn,j,1] == 1 and state[turn,j,2] == 1:
+                if state[turn, j, 0] == 1 and state[turn, j, 1] == 1 and state[turn, j, 2] == 1:
                     return 1
         if state.sum() >= 9:
             return 0
@@ -64,22 +64,21 @@ class TicTacToeEnv(object):
         return 2
 
     def print_board(self, state):
-        print('print_board')
         for i in range(3):
             row = ''
             for j in range(3):
-                if state[0,i,j] == 1:
+                if state[0, i, j] == 1:
                     row += 'x '
-                elif state[1,i,j] == 1:
+                elif state[1, i, j] == 1:
                     row += 'o '
                 else:
                     row += '* '
             print(row)
 
     def convert_action_to_int(self, action_array):
-        '''
+        """
         action_array is 2x3x3
-        '''
+        """
         action_array = np.reshape(action_array, -1)
         return np.where(action_array == 1)[0]
 
@@ -91,12 +90,12 @@ class TicTacToeEnv(object):
         return action_array
 
     def get_turn_tictactoe(self, state):
-        '''
-        return 0 if x's turn 
+        """
+        return 0 if x's turn
         return 1 if o's turn
-        '''
-        num_xs = state[0,:,:].sum()
-        num_os = state[1,:,:].sum()
+        """
+        num_xs = state[0, :, :].sum()
+        num_os = state[1, :, :].sum()
         if num_os == num_xs:
             return 0
         elif num_xs > num_os:
