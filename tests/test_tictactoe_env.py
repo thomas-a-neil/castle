e66d1a17b1d89e6649bee599e76d65961f40a5cb
Neil Thomas <thomas.a.neil@gmail.com>
2017-11-25 22:10:56 -0800

    Formatting in tictactoe test

--- tests/test_tictactoe_env.py
+++ tests/test_tictactoe_env.py
@@ -97,57 +97,50 @@ class TestTicTacToeEnv(unittest.TestCase):
         start_state = np.zeros((2, 3, 3), dtype=int)
         self.env.print_board(start_state)
         action_array = np.zeros((2, 3, 3), dtype=int)
-        action_array[0,0,1] = 1
+        action_array[0, 0, 1] = 1
         action_int = self.env.convert_action_to_int(action_array)
         next_state = self.env.get_next_state(start_state, action_int)
 
         self.env.print_board(next_state)
 
         action_array = np.zeros((2, 3, 3), dtype=int)
-        action_array[1,1,1] = 1
+        action_array[1, 1, 1] = 1
         action_int = self.env.convert_action_to_int(action_array)
         next_state = self.env.get_next_state(next_state, action_int)
         self.env.print_board(next_state)
 
         action_array = np.zeros((2, 3, 3), dtype=int)
-        action_array[0,2,2] = 1
+        action_array[0, 2, 2] = 1
         action_int = self.env.convert_action_to_int(action_array)
         next_state = self.env.get_next_state(next_state, action_int)
 
         action_array = np.zeros((2, 3, 3), dtype=int)
-        action_array[1,2,0] = 1
-        action_int = self.env.convert_action_to_int(action_array)
-        next_state = self.env.get_next_state(next_state, action_int)
-
-        self.assertEqual(self.env.is_game_over(next_state), 0)
-
-        action_array = np.zeros((2, 3, 3), dtype=int)
-        action_array[0,0,0] = 1
+        action_array[1, 2, 0] = 1
         action_int = self.env.convert_action_to_int(action_array)
         next_state = self.env.get_next_state(next_state, action_int)
 
         self.assertEqual(self.env.is_game_over(next_state), 0)
 
         action_array = np.zeros((2, 3, 3), dtype=int)
-        action_array[1,0,2] = 1
+        action_array[0, 0, 0] = 1
+        action_int = self.env.convert_action_to_int(action_array)
+        next_state = self.env.get_next_state(next_state, action_int)
+
+        self.assertEqual(self.env.is_game_over(next_state), 0)
+
+        action_array = np.zeros((2, 3, 3), dtype=int)
+        action_array[1, 0, 2] = 1
         action_int = self.env.convert_action_to_int(action_array)
         next_state = self.env.get_next_state(next_state, action_int)
 
         true_next_state = np.zeros((2, 3, 3), dtype=int)
-        true_next_state[0,0,1] = 1
-        true_next_state[1,1,1] = 1
-        true_next_state[0,0,0] = 1
-        true_next_state[0,2,2] = 1
-        true_next_state[1,2,0] = 1
-        true_next_state[1,0,2] = 1
+        true_next_state[0, 0, 1] = 1
+        true_next_state[1, 1, 1] = 1
+        true_next_state[0, 0, 0] = 1
+        true_next_state[0, 2, 2] = 1
+        true_next_state[1, 2, 0] = 1
+        true_next_state[1, 0, 2] = 1
 
         self.assertTrue(np.array_equal(true_next_state, next_state))
         self.assertEqual(self.env.is_game_over(next_state), 1)
         self.assertEqual(self.env.outcome(next_state), -1)
-
-
-
-
-
-        
-
